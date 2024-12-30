58cb01bc369bf96feb47c53704ca050879b89f39
bilalkolli <bilal.kokoko@outlook.fr>
2024-12-30 16:47:17 +0100

    feat: add default color palette to TailwindCSS configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,12 @@ export default {
   content: [],
   theme: {
     extend: {},
+    colors: {
+      'white': '#FFFFFF',
+      'athensGray': '#E5E7EB',
+      'seaBuckthorn': '#F79F1A',
+      'black': '#000000'
+    },
   },
   plugins: []
 }
