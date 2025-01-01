58594d9fcf4d35b85d601b25a75e6511c8136fcb
bilalkolli <bilal.kokoko@outlook.fr>
2025-01-01 21:50:39 +0100

    feat: add dynamic recipe detail page at recipes/[id].vue

---
+++ pages/recipes/[id].vue
@@ -0,0 +1,28 @@
+<script setup lang="ts">
+import { useRoute } from 'vue-router'
+import type { IRecipeDetail } from '~/types/IRecipeDetail'
+
+definePageMeta({
+  layout: 'landing'
+})
+
+const route = useRoute()
+const recipeId = route.params.id as string
+
+const { data } = useFetch<IRecipeDetail>(`https://dummyjson.com/recipes/${recipeId}`)
+</script>
+
+
+<template>
+  <RecipeDetails
+  v-if="data"
+  :name="data?.name"
+  :cookTimeMinutes="data?.cookTimeMinutes"
+  :caloriesPerServing="data?.caloriesPerServing"
+  :rating="data?.rating"
+  :reviewCount="data?.reviewCount"
+  :image="data?.image"
+  :ingredients="data?.ingredients"
+  :instructions="data?.instructions"
+  />
+</template>
